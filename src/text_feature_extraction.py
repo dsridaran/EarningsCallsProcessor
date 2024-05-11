a8c14f0ff0b5d81b1bf14417122c8fc43b79ef77
Dilan SriDaran <44761051+dsridaran@users.noreply.github.com>
2024-05-10 23:09:17 -0400

    Update text_feature_extraction.py

--- src/text_feature_extraction.py
+++ src/text_feature_extraction.py
@@ -8,12 +8,8 @@ from nltk.tokenize import sent_tokenize
 from bertopic.representation import KeyBERTInspired
 import spacy
 
-
-
-# Calculate readability metrics
 def calculate_readability_metrics(df, text_var, keep_all = False):
 
-    # Define readability functions
     readability_functions = {
         'automated_readability_index': textstat.automated_readability_index,
         'coleman_liau_index': textstat.coleman_liau_index,
@@ -24,63 +20,38 @@ def calculate_readability_metrics(df, text_var, keep_all = False):
         'smog_index': textstat.smog_index,
     }
 
-    # Evaluate readability metrics
     for metric, function in readability_functions.items():
         df[f'r_{metric}_{text_var}'] = df[text_var].apply(function)
 
-    # Calculate overall readability
     metric_columns = [f'r_{metric}_{text_var}' for metric in readability_functions]
     df[f'readability_overall_{text_var}'] = df[metric_columns].mean(axis = 1)
 
-    # Drop columns (if required)
     if not keep_all:
-        df.drop(metric_columns, axis=1, inplace=True)
-        if not keep_all:
-            df.drop([text_var], axis=1, inplace=True)
+        df.drop(metric_columns, axis = 1, inplace = True)
+        df.drop([text_var], axis = 1, inplace = True)
 
-    # end
     return df
 
-# Create readability features
 def create_readability_features(df):
-
-    # Aggregate data by speaker
     grouped = df.groupby(['company_name', 'date', 'speaker_type', 'type'])['text'].apply(' '.join).reset_index()
-
-    # Pivot data to wide format
     pivot_table = grouped.pivot(index = ['company_name', 'date'], columns = ['speaker_type', 'type'], values = 'text').reset_index()
     pivot_table = pivot_table.fillna('')
     pivot_table.columns = ['company_name', 'date', 'conf_qna', 'corp_pres', 'corp_qna', 'op_pres', 'op_qna']
-
-    # Calculate readability by speaker type
     for var in ['conf_qna', 'corp_pres', 'corp_qna', 'op_pres', 'op_qna']:
         pivot_table = calculate_readability_metrics(pivot_table, text_var = var)
-
-    # end
     return pivot_table
 
-
-
-# Load the spaCy model
 nlp = spacy.load('en_core_web_sm')
 nltk.download('punkt')
 
-# Redact text for topic models
 def redact_text_with_spacy(text):
-
-    # Process text
     doc = nlp(text)
     redacted_text = text
     sorted_entities = sorted(doc.ents, key = lambda ent: ent.start_char, reverse = True)
-
-    # Replace entities with their label
     for ent in sorted_entities:
         if ent.label_ not in ['ORDINAL', 'CARDINAL']:
             redacted_text = redacted_text[:ent.start_char] + '[' + ent.label_ + ']' + redacted_text[ent.end_char:]
-
     return redacted_text, text
-
-
 
 def create_sentence_topics(df):
 
